d2c300ffe412062b45ce1591ee5181d75287b57b
Neil <neil.bau@tista.net>
2025-10-17 16:15:12 +0800

    feat: create user model

--- ride_app/models.py
+++ ride_app/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from ride_app.mixins import BaseModelMixin
+
+
+class UserTypeChoices(models.TextChoices):
+    ADMIN = "Admin", "Admin"
+    RIDER = "Rider", "Rider"
+    DRIVER = "Driver", "Driver"
+
+
+class User(BaseModelMixin, AbstractUser):
+    phone_number = models.CharField(max_length=255, db_index=True)
+    email = models.EmailField(null=True, blank=True, db_index=True)
+    user_type = models.CharField(
+        max_length=50,
+        choices=UserTypeChoices.choices,
+        null=True,
+        blank=True,
+        db_index=True,
+    )
