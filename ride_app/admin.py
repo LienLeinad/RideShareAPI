42906270f39f775085bcb20d01bc499f68112877
Neil <neil.bau@tista.net>
2025-10-17 16:47:24 +0800

    misc: add admin for RideEvent

--- ride_app/admin.py
+++ ride_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from ride_app.models import Ride, User
+from ride_app.models import Ride, RideEvent, User
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Ride)
+admin.site.register(RideEvent)
