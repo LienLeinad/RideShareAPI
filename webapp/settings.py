aedabe42921ae302ae4f32ffe4a1aa12d13ec7c0
Neil <neil.bau@tista.net>
2025-10-17 18:15:59 +0800

    chore: add django extensions

--- webapp/settings.py
+++ webapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "django_extensions",
     "rest_framework",
     "rest_framework_simplejwt",
     "ride_app",
