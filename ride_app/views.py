89b9ce0f76ecd571b1a3f9003f266b151245929e
Neil <neil.bau@tista.net>
2025-10-20 22:00:42 +0800

    feat[WIP]:init filter code

--- ride_app/views.py
+++ ride_app/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.views import LoginView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
@@ -15,6 +17,11 @@ class RideViewSet(ModelViewSet):
     queryset = (
         Ride.objects.all().prefetch_related("events").select_related("rider", "driver")
     )
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    ordering_fields = [
+        "pickup_time",
+    ]
+    ordering = ["created_at"]
 
 
 class CustomLoginView(LoginView):
