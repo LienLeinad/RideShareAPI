96b77f87f5d6a4e84c0ff9567ad32e2832f734c4
Neil Bautista <33220418+LienLeinad@users.noreply.github.com>
2025-10-20 20:29:57 +0800

    Feat/ride api (#3)
    * feat: initial serializer code
    
    * feat: initial Viewset
    
    * misc: move select related to one call
    
    * test: finalize unittest
    
    * chore: create debug view
    
    * feat: optimize query
    
    * remove pagination for now
    
    * Revert "chore: create debug view"
    
    This reverts commit be6500d5883d54c02e73dc333fc7e27e5a9c1688.
    
    * chore: finalize test data generation scripts
    
    * chore: optimize script
    
    * chore: document sript
    
    * chore: document sript
    
    * chore: delete unneccessary scripts

--- ride_app/serializers.py
+++ ride_app/serializers.py
@@ -1,8 +1,10 @@
+from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import ModelSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from ride_app.models import UserRoleChoices
+from ride_app.models import Ride, RideEvent, User, UserRoleChoices
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -13,3 +15,41 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         if self.user.role and self.user.role != UserRoleChoices.ADMIN.value:
             raise ValidationError("User is not allowed to use the API.")
         return data
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = [
+            "password",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "updated_at",
+            "created_at",
+            "last_login",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+        ]
+
+
+class RideEventSerializer(ModelSerializer):
+    ride = serializers.SerializerMethodField()
+
+    def get_ride(self, obj: RideEvent):
+        return obj.ride.id
+
+    class Meta:
+        model = RideEvent
+        fields = "__all__"
+
+
+class RideSerializer(ModelSerializer):
+    rider = UserSerializer(required=False)
+    driver = UserSerializer(required=False)
+    todays_ride_events = RideEventSerializer(many=True, source="events")
+
+    class Meta:
+        model = Ride
+        fields = "__all__"
---
+++ scripts/generate_test_data.py
@@ -0,0 +1,72 @@
+"""
+    Generates n instances of rider/driver pairs,n rides per pair, and n*3 ride events per ride
+    and 1 admin user
+    For example:
+        n = 3
+        total new users = 6 (3 riders, 3 Drivers)
+        total new rides = 36 (6 per rider/driver pair)
+        total new ride events = 98 (3 per ride)
+    usage:
+    python manage.py runscript scripts.generate_test_data_alot --script-args <n>
+"""
+
+from django.utils import timezone
+
+from ride_app.models import Ride, RideEvent, RideStatusChoices, User, UserRoleChoices
+
+
+def run(*args):
+    # Clear everything to keep db clean
+    User.objects.exclude(is_superuser=True).delete()
+    Ride.objects.all().delete()
+    RideEvent.objects.all().delete()
+    iters = int(args[0])
+    # Create an admin to test authentication
+    User.objects.create(
+        username="test_admin",
+        password="Test1234!",
+        first_name="test",
+        last_name="admin",
+        phone_number="+639171234123",
+        role=UserRoleChoices.ADMIN.value,
+    )
+    for i in range(iters):
+        rider = User.objects.create(
+            username=f"test{i}",
+            password="test1234!",
+            first_name=f"test{i}",
+            last_name="test",
+            phone_number="+639171231234",
+            role=UserRoleChoices.RIDER.value,
+        )
+        driver = User.objects.create(
+            username=f"test_driver{i}",
+            password="test1234!",
+            first_name=f"test{i}",
+            last_name="driver",
+            phone_number="+639271231234",
+            role=UserRoleChoices.DRIVER.value,
+        )
+
+        for j in range(iters):
+            ride = Ride.objects.create(
+                rider=rider,
+                driver=driver,
+                pickup_latitude=123.0,
+                pickup_longitude=123.0,
+                dropoff_latitude=321.0,
+                dropoff_longitude=321.0,
+                pickup_time=timezone.now(),
+                status=RideStatusChoices.DROP_OFF.value,
+            )
+            ride.events.create(description="Picked Up", created_at=timezone.now())
+            ride.events.create(
+                ride=ride,
+                description="En Route",
+                created_at=timezone.now(),
+            )
+            ride.events.create(
+                ride=ride,
+                description="Dropped Off",
+                created_at=timezone.now(),
+            )
