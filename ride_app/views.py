0f57aca9b60e9cb4e1ed964fb17d5212903386e4
Neil <neil.bau@tista.net>
2025-10-22 17:44:29 +0800

    feat: add exception handling for ordering driver distance without query params

--- ride_app/views.py
+++ ride_app/views.py
@@ -5,6 +5,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet
 
 from ride_app.forms import CustomAuthForm
@@ -58,6 +60,20 @@ class RideViewSet(ModelViewSet):
         if longitude and latitude:
             qs = self.annotate_driver_distance(qs, float(longitude), float(latitude))
         return qs
+
+    def list(self, request, *args, **kwargs):
+        longitude = self.request.query_params.get("driver_longitude")
+        latitude = self.request.query_params.get("driver_latitude")
+        # Handle case where ordering is checked for driver_distance but no long/lat is provided
+        ordering = self.request.query_params.get("ordering")
+        if not longitude and not latitude and ordering == "driver_distance":
+            return Response(
+                {
+                    "detail": "Cannot order using driver distance without provided longitude and latitude in query parameters."
+                },
+                status=HTTP_400_BAD_REQUEST,
+            )
+        return super().list(request, *args, **kwargs)
 
 
 class CustomLoginView(LoginView):
